32500d7ff0e7de0c460361bf01e581ff4e798bd1
ShnMndz <shanenorwin.mendoza@tup.edu.ph>
2023-10-10 08:33:45 +0700

    Initial commit

---
+++ backend/routes/product.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const router = express.Router();
+
+const {getProducts,newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
+// const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+
+router.get('/products', getProducts)
+router.post('/product/new', newProduct)
+router.get('/product/:id', getSingleProduct);
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+
+
+module.exports = router;
